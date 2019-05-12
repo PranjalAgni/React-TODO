1c06bee534ea4978c25731371389452b67212d83
Pranjal Agnihotri <pranjal.agnihotri6@gmail.com>
2019-05-12 12:05:06 +0530

    :tada: first commit with basic todo app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      message: 'Hello From React!',
+      newTodo: '',
+      todos: [
+        {
+          title: 'Learn React',
+          done: false
+        },
+        {
+          title: 'Learn JSX',
+          done: false
+        }
+      ]
+    };
+  }
+
+  formSubmitted(event) {
+    event.preventDefault();
+    this.setState({
+      newTodo: '',
+      todos: [
+        ...this.state.todos,
+        {
+          title: this.state.newTodo,
+          done: false
+        }
+      ]
+    });
+  }
+
+  newTodoChanged(event) {
+    this.setState({
+      newTodo: event.target.value
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>{this.state.message}</h1>
+        <form onSubmit={this.formSubmitted.bind(this)}>
+          <label htmlFor="newTodo">New Todo</label>
+          <input
+            onChange={this.newTodoChanged.bind(this)}
+            id="newTodo"
+            name="newTodo"
+            value={this.state.newTodo}
+          />
+
+          <button type="submit">Add Todo</button>
+        </form>
+        <ul>
+          {this.state.todos.map(todo => {
+            return <li key={todo.title}>{todo.title}</li>;
+          })}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
